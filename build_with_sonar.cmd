199f86d29511dca859af2da946932dc9c390f74e
gilzilberfeld <gil.zilberfeld@gmail.com>
2023-05-08 11:03:45 +0300

    initial

---
+++ build_with_sonar.cmd
@@ -0,0 +1,5 @@
+clean verify sonar:sonar \
+  -Dsonar.projectKey=CleanCode \
+  -Dsonar.projectName='CleanCode' \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.token=sqp_2bfbc94ed817120e0cdf873be83e575ac1622ea2
---
+++ src/test/java/webinars/testingil/cleancodesonar/GildedRoseApprovalTest.java
@@ -0,0 +1,60 @@
+package webinars.testingil.cleancodesonar;
+
+import org.approvaltests.reporters.DiffReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.junit.jupiter.api.Test;
+import webinars.testingil.cleancodesonar.after.GildedRose;
+import webinars.testingil.cleancodesonar.after.Item;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.lang.String.valueOf;
+import static org.approvaltests.Approvals.verify;
+
+@UseReporter(DiffReporter.class)
+public class GildedRoseApprovalTest {
+    String log = "";
+    String itemLog;
+
+    Item[] items = new Item[]{
+            new Item("+5 Dexterity Vest", 10, 20),
+            new Item("Aged Brie", 2, 0),
+            new Item("Elixir of the Mongoose", 5, 7),
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
+    };
+
+    @Test
+    public void gilded_rose_30_days() {
+
+
+        var shop = new GildedRose(items);
+
+        IntStream.range(1, 30).forEach(day -> {
+                    log += "Day " + valueOf(day) + "\n";
+                    log += printItems();
+                    shop.updateQuality();
+                    log += "\n";
+                }
+        );
+
+
+        verify(log);
+    }
+    public String printItems() {
+        itemLog = "name, sellIn, quality\n";
+        Arrays.asList(items).forEach(item -> {
+            itemLog += (item.toString() + "\n");
+        });
+        itemLog += "\n";
+
+        return itemLog;
+    }
+
+
+}
